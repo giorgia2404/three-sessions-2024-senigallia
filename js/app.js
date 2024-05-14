1cd48ebb80db519bf53f686d59e770e39d4d6b72
Daniele T <Fupete@users.noreply.github.com>
2024-05-14 18:53:17 +0200

    feat: mirror primer

--- js/app.js
+++ js/app.js
@@ -343,6 +343,7 @@ const loadTexture = (name, path, format) => {
 // Let's preload our textures
 // global.cubeTextures[n]
 loadCubeTexture('PureSky', './assets/textures/cube/PureSky-256/', '.png') // 0
+loadCubeTexture('PureSky', './assets/textures/cube/AbandonedParking-1024/', '.png') // 1
 // ...
 // global.textures[n]
 loadTexture('StoneDiff', './assets/textures/stone_tiles_02_diff_1k', '.jpg') // 0
