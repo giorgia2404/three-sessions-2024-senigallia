7a521c8475319bbe502ebeaa3d6d67c3891507c6
Daniele T <Fupete@users.noreply.github.com>
2024-05-26 16:43:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -387,7 +387,7 @@ const randomSketch = () => {
 	*/
 	const sets = [0, 1, 2, 3, 4] // available sets
 	const setsWeights = [0.25, 0.25, 0.25, 0, 0.25] // available sets: probability weight
-	const sketches = [1, 1, 1, 0, 2] // no. of available sketches for each set
+	const sketches = [1, 1, 1, 0, 3] // no. of available sketches for each set
 	// playSet
 	// pure random
 	// global.playSet = Math.round(Math.random() * (sets.length - 1));
